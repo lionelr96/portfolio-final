738b21d2614edd8f475388ce2205c49787ba67c2
Lionel Robert <lionelr@umich.edu>
2020-12-08 12:45:39 -0500

    extra thing

---
+++ script/app.js
@@ -0,0 +1,27 @@
+function addListeners(parentClass) {
+	const focusableItems = [
+		"a[href]",
+		"area[href]",
+		"input:not([disabled])",
+		"select:not([disabled])",
+		"textarea:not([disabled])",
+		"button:not([disabled])",
+		'[tabindex="0"]'
+	];
+	for (i = 0, leni = focusableItems.length; i < leni; i++) {
+		let focusableElements = document.querySelectorAll(
+			"." + parentClass + " " + focusableItems[i]
+		);
+		for (j = 0, lenj = focusableElements.length; j < lenj; j++) {
+			focusableElements[j].addEventListener("focus", function() {
+				document.querySelector("." + parentClass).classList.add("focus-within");
+			});
+			focusableElements[j].addEventListener("blur", function() {
+				document
+					.querySelector("." + parentClass)
+					.classList.remove("focus-within");
+			});
+		}
+	}
+}
+addListeners("fliip-card");
